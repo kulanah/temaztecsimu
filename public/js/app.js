5485acd24c05d176c4387979a4e00dd7fdcc1ec5
kulanah <devonthebutler@gmail.com>
2017-04-25 12:15:09 -0700

    added absolute placed extract val

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,15 @@ $('#colValvesClosed').click(function(){
   alert('test complete');
 });
 
+$('#extractUp').click(function(){
+  extractVal += 100;
+  $('#testVal').text(extractVal + "V  ");
+  console.log(extractVal);
+});
 
 
-
+$('#extractDown').click(function(){
+  extractVal -= 100;
+  $('#testVal').text(extractVal + "V");
+  console.log(extractVal);
+});
