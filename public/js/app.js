b7555ec949c4f52bbbba12681e35ba2558e3cced
kulanah <devonthebutler@gmail.com>
2017-04-26 11:01:07 -0700

    removed extraneous files AND set current as default

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,45 @@
-let extractVal = 0;
+'use strict';
+
+let gunLense = 1;
+let extractVal = 3500;
+let extractFinal = 4500;
 
 $('#colValvesClosed').click(function(){
   alert('test complete');
 });
 
 $('#extractUp').click(function(){
-  extractVal += 100;
-  $('#testVal').text(extractVal + "V");
-  console.log(extractVal);
+  if (extractVal < 4500){
+    extractVal += 100;
+    $('#extractVal').text(extractVal + " V");
+  }
 });
 
 
 $('#extractDown').click(function(){
-  extractVal -= 100;
-  $('#testVal').text(extractVal + "V");
-  console.log(extractVal);
+  if (extractVal > 3000){
+    extractVal -= 100;
+    $('#extractVal').text(extractVal + " V");
+  }
 });
+
+$('#extractSubmit').click(function(){
+  extractFinal = extractVal;
+  $('#extractFinal').text(extractFinal + " V");
+});
+
+$('#gunLenseDown').click(function(){
+  if (gunLense > 1){
+    --gunLense;
+  }
+  //min 1 max 7
+  $('#gunLenseVal').text(gunLense);
+});
+
+$('#gunLenseUp').click(function(){
+  if(gunLense < 7){
+    ++gunLense;
+    $('#gunLenseVal').text(gunLense);
+  }
+});
+
