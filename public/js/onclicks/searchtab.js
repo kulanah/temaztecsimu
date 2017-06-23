4cc28e2c221e19e2256cdda3a3068439c65a0dcf
Devon <devonthebutler@gmail.com>
2017-06-22 17:34:39 -0700

    fixed bug with micronano buttons in search tab

--- public/js/onclicks/searchtab.js
+++ public/js/onclicks/searchtab.js
@@ -3,14 +3,14 @@
 let micro = true;
 let searchTab = function(){
   $('#nano').on('click', function(event){
-    $('#leftcolumn').attr('src', './public/img/leftcolsearchmicro.png');
+    $('#leftcolumnbeamsettings').attr('src', './public/img/beamsettingsmicro.png');
     beamslider.val(3);
     $('#beamvalue').text(beamslider.val());
     micro = true;
   });
 
   $('#micro').on('click', function(event){
-    $('#leftcolumn').attr('src', './public/img/leftcolsearchnano.png')
+    $('#leftcolumnbeamsettings').attr('src', './public/img/beamsettingsnano.png')
     beamslider.val(6);
     $('#beamvalue').text(beamslider.val());
     micro = false;
