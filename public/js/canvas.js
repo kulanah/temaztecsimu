3784ba21270f5a4d159d477f5163feaef744d3b8
Devon <devonthebutler@gmail.com>
2017-07-06 09:26:05 -0700

    cleaned up canvas class

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -1,44 +1,37 @@
 'use strict';
 
+//this allows me to call an external function upon image load,  this could definitely be done bettter but I'm not sure how
 let contextVar;
-class Canvas {
 
+class Canvas {
   constructor(source, cssID){
     this.selector = $('#' + cssID);
     this.context = this.selector[0].getContext('2d');
-    this.baseContext = this;
 
     contextVar = this;
 
-
-    this.maskX = 0;
-    this.maskY = 0;
-    this.maskR = 0;
-
     this.img = new Image;
     this.img.src = source;
+    this.imgX = 0;
+    this.imgY = 0;
 
     this.img.onload = function(){
       console.log(this);
       contextVar.setMaskXYR();
     }
 
-    console.log(this.maskX);
+    this.maskX = 0;
+    this.maskY = 0;
+    this.maskR = 0;
 
     this.zooms = [0.25, 0.5, 1.0, 2.0, 4.0];
-    
-    this.imgX = 0;
-    this.imgY = 0;
   };
 
   setMaskXYR(){
     this.maskX = this.img.width / 2;
     this.maskY = this.img.height / 2;
     this.maskR = this.img.width / 8;
-
-
   }
-
 
   drawCanvas(){
     this.context.clearRect(0,0,this.img.width,this.img.height);
