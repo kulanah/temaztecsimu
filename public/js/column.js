523d392d3ff97e6ce248f4d298a8a6340fe1d0bc
Devon <devonthebutler@gmail.com>
2017-08-09 11:37:11 -0700

    changed fucntion vars to just ints

--- public/js/column.js
+++ public/js/column.js
@@ -20,32 +20,31 @@ beamLabels.css('position', 'absolute');
 beamLabels.css('top', '0');	
 
 //                  xCenter x  y      f       kind     name
-temLens[0] = new lens(()=>0,0,()=>20,()=>-8,'source','Electron Gun');
-temLens[1] = new lens(()=>0,0,()=>90,()=>25,'lens','Gun lens');
-temLens[2] = new lens(()=>0,0,()=>130,()=>140,'lens','Gun Deflection 1');
-temLens[3] = new lens(()=>0,0,()=>160,()=>14,'lens','Gun Deflection 2');
-temLens[4] = new lens(()=>0,0,()=>220,()=>50,'lens','Condenser Lens 1');
-temLens[5] = new lens(()=>0,0,()=>260,()=>6.1,'aperture','C1 Aperture');
-temLens[6] = new lens(()=>0,0,()=>320,()=>10,'lens','Condenser Lens 2');
-temLens[7] = new lens(()=>0,0,()=>350,()=>25,'aperture','C2 Aperture');
-temLens[8] = new lens(()=>0,0,()=>400,()=>100, 'lens','Beam Deflection 1');
-temLens[9] = new lens(()=>0,0,()=>440,()=>15,'lens','Beam Deflection 2');
-temLens[10] = new lens(()=>0,0,()=>480,()=>24,'lens','Minicondenser Lens');
-temLens[11] = new lens(()=>0,0,()=>520,()=>70,'lens','Objective Condenser Lens');
-temLens[12] = new lens(()=>0,0,()=>570,()=>-10,'sample','Specimen');
-temLens[13] = new lens(()=>0,0,()=>600,()=>40,'lens','Objective Imaging Lens');
-temLens[14] = new lens(()=>0,0,()=>650,()=>8,'aperture','Objective Aperture');
-temLens[15] = new lens(()=>0,0,()=>700,()=>100,'lens','Image Deflection 1');
-temLens[16] = new lens(()=>0,0,()=>750,()=>30,'lens','Image Deflection 2');
-temLens[17] = new lens(()=>0,0,()=>800,()=>58,'lens','Intermediate Lens');
-temLens[18] = new lens(()=>0,0,()=>840,()=>24,'aperture','Diffraction Aperture');
-temLens[19] = new lens(()=>0,0,()=>880,()=>10,'lens','Projection lens 1');
-temLens[20] = new lens(()=>0,0,()=>990,()=>200,'lens','Projection Lens 2');
-temLens[21] = new lens(()=>0,0,()=>1160,()=>400,'screen','Viewing Screen');
+temLens[00] = new lens(0,0,20,8,'source','Electron Gun');
+temLens[01] = new lens(0,0,90,25,'lens','Gun lens');
+temLens[02] = new lens(0,0,130,140,'lens','Gun Deflection 1');
+temLens[03] = new lens(0,0,160,14,'lens','Gun Deflection 2');
+temLens[04] = new lens(0,0,220,50,'lens','Condenser Lens 1');
+temLens[05] = new lens(0,0,260,6.1,'aperture','C1 Aperture');
+temLens[06] = new lens(0,0,320,10,'lens','Condenser Lens 2');
+temLens[07] = new lens(0,0,350,25,'aperture','C2 Aperture');
+temLens[08] = new lens(0,0,400,100, 'lens','Beam Deflection 1');
+temLens[09] = new lens(0,0,440,15,'lens','Beam Deflection 2');
+temLens[10] = new lens(0,0,480,24,'lens','Minicondenser Lens');
+temLens[11] = new lens(0,0,520,70,'lens','Objective Condenser Lens');
+temLens[12] = new lens(0,0,570,10,'sample','Specimen');
+temLens[13] = new lens(0,0,600,40,'lens','Objective Imaging Lens');
+temLens[14] = new lens(0,0,650,8,'aperture','Objective Aperture');
+temLens[15] = new lens(0,0,700,100,'lens','Image Deflection 1');
+temLens[16] = new lens(0,0,750,30,'lens','Image Deflection 2');
+temLens[17] = new lens(0,0,800,58,'lens','Intermediate Lens');
+temLens[18] = new lens(0,0,840,24,'aperture','Diffraction Aperture');
+temLens[19] = new lens(0,0,880,10,'lens','Projection lens 1');
+temLens[20] = new lens(0,0,990,200,'lens','Projection Lens 2');
+temLens[21] = new lens(0,0,1160,400,'screen','Viewing Screen');
 
 
 //creates the labels for the lenses
-let totalHeight = 0;
 visualArray = new Array;
 for(i = 0; i < temLens.length; i++){
 	visualArray[i] = document.createElement('div');
@@ -77,7 +76,6 @@ function drawColumn(){
 
 	let xScale = 1;
 	let yScale = canvasHeight / 1160;
-	console.log(yScale$)
 
 	beamLabels[0].getContext('2d').scale(xScale, yScale);
 	beamDiag[0].getContext('2d').scale(xScale, yScale);
@@ -90,8 +88,8 @@ function drawColumn(){
 	var offset = 120;
 	//draw CenterLine
 	ctx.beginPath();
-	ctx.moveTo(offset,temLens[0].y());
-	ctx.lineTo(offset,temLens[temLens.length-1].y());  
+	ctx.moveTo(offset,temLens[0].y);
+	ctx.lineTo(offset,temLens[temLens.length-1].y);  
 	ctx.strokeStyle = 'blue';
 	ctx.stroke();
 	
@@ -100,24 +98,24 @@ function drawColumn(){
 	for(var ray=0; ray<numOfRays+1; ray++){
 		temLens[0].x=  ray*widthOfSource/numOfRays - widthOfSource/2;
 		ctx.beginPath();
-		ctx.moveTo(temLens[0].x+offset,temLens[0].y());
+		ctx.moveTo(temLens[0].x+offset,temLens[0].y);
 		var blocked=0;
 		for(var i=0;i<numOfLenses;i++){
 			if(i>0){
 				if(temLens[i-1].kind=='lens' || temLens[i-1].kind=='source' || temLens[i-1].kind=='sample'){
-					temLens[i].x = temLens[i-1].x-(temLens[i].y() -temLens[i-1].y())*((temLens[i-1].x-temLens[i-1].xCenter())/temLens[i-1].f());
+					temLens[i].x = temLens[i-1].x-(temLens[i].y - temLens[i-1].y)*((temLens[i-1].x-temLens[i-1].xCenter)/temLens[i-1].f);
 				}	
 				if(temLens[i-1].kind == 'aperture'){
-					if(temLens[i-1].x < -temLens[i-1].f() +temLens[i-1].xCenter() || temLens[i-1].x>temLens[i-1].f() +temLens[i-1].xCenter()){
+					if(temLens[i-1].x < -temLens[i-1].f + temLens[i-1].xCenter || temLens[i-1].x>temLens[i-1].f +temLens[i-1].xCenter){
 						blocked = 1;	
 					}else{
-						temLens[i].x = temLens[i-2].x-(temLens[i].y()-temLens[i-2].y())*((temLens[i-2].x-temLens[i-2].xCenter())/temLens[i-2].f());
+						temLens[i].x = temLens[i-2].x-(temLens[i].y-temLens[i-2].y)*((temLens[i-2].x-temLens[i-2].xCenter)/temLens[i-2].f);
 					}
 				}	
 			}
 			if(blocked==0){
 				
-				ctx.lineTo(temLens[i].x+offset,temLens[i].y());  
+				ctx.lineTo(temLens[i].x+offset,temLens[i].y);  
 				
 			}
 		}		
@@ -134,25 +132,25 @@ function drawColumn(){
 	ctx = beamDiag[0].getContext('2d');
 	for(var i=0;i<numOfLenses;i++){
 		if (i == 0){
-			totalHeight = temLens[i].y();
+			totalHeight = temLens[i].y;
 		} else {
-			totalHeight += temLens[i].y() - temLens[i - 1].y();
+			totalHeight += temLens[i].y - temLens[i - 1].y;
 		}
 		if(temLens[i].kind=='sample'){
 			ctx.fillStyle = '#fff';
-			ctx.fillRect(offset-60,temLens[i].y(),180,6);
+			ctx.fillRect(offset-60,temLens[i].y,180,6);
 		}
 		if(temLens[i].kind=='lens'){
 			ctx.fillStyle = '#fff';
-			ctx.fillRect(offset-114,temLens[i].y()-12,14,24);
-			ctx.fillRect(offset+100,temLens[i].y()-12,14,24);
+			ctx.fillRect(offset-114,temLens[i].y-12,14,24);
+			ctx.fillRect(offset+100,temLens[i].y-12,14,24);
 		}
 		if(temLens[i].kind=='aperture'){
 			ctx.beginPath();
-			ctx.moveTo(offset+temLens[i].f()+temLens[i].xCenter(),temLens[i].y());
-			ctx.lineTo(offset+114,temLens[i].y()); 
-			ctx.moveTo(offset-temLens[i].f()+temLens[i].xCenter(),temLens[i].y());
-			ctx.lineTo(offset-114,temLens[i].y()); 
+			ctx.moveTo(offset+temLens[i].f+temLens[i].xCenter,temLens[i].y);
+			ctx.lineTo(offset+114,temLens[i].y); 
+			ctx.moveTo(offset-temLens[i].f+temLens[i].xCenter,temLens[i].y);
+			ctx.lineTo(offset-114,temLens[i].y); 
 			ctx.strokeStyle = '#fff';
 			ctx.stroke();
 		}
@@ -162,19 +160,18 @@ function drawColumn(){
 
 		if(temLens[i].kind=='screen'){
 			visualArray[i].style.left = offset + 120 + 'px';
-			visualArray[i].style.top = temLens[i].y() - 4 * yScale + 'px';
+			visualArray[i].style.top = temLens[i].y - 4 * yScale + 'px';
 		
 		}else{
       if(temLens[i].kind=='source'){
         visualArray[i].style.left = offset + 95 + 'px';
-        visualArray[i].style.top = temLens[i].y() - 4 + 'px';
+        visualArray[i].style.top = temLens[i].y - 4 + 'px';
       }else{
         visualArray[i].style.left = offset + 95 + 'px';
-				visualArray[i].style.top = (temLens[i].y() * yScale - 8 ) + 'px';
+				visualArray[i].style.top = (temLens[i].y * yScale - 8 ) + 'px';
       }
     }
 		columnDiv[0].append(visualArray[i]);
 	}
 	ctx.restore();	
-	console.log(totalHeight);
 }
