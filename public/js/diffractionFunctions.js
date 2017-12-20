711e09ff1e28afc3a77daa59bc0831a727dfdd9d
Noah Freed <noah25@psu.ds.pdx.edu>
2017-12-20 10:19:50 -0800

    Increased number of layers for diffraction, increased d spacing

--- public/js/diffractionFunctions.js
+++ public/js/diffractionFunctions.js
@@ -95,7 +95,7 @@ function calculateR1R2Angle(material, u, v, w, voltage, cameraLength, max) {
                                     }
                                     var lambda = 12.27 / Math.sqrt(voltage * (1 + 0.000000978 * voltage));
                                     function calculateR(d) {
-                                        return lambda * cameraLength * 1000 / d / 10000;
+                                        return lambda * cameraLength / d / 5;
                                     }
                                     var r1 = calculateR(d1);
                                     var r2 = calculateR(d2);
