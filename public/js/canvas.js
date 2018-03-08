b61644df3a0aaf2b0d2cf654c58bf876743f10c2
Noah Freed <noah25@psu.ds.pdx.edu>
2018-03-08 15:22:50 -0800

    Halo no longer appears outside of SAED aperture

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -758,6 +758,14 @@ class Canvas {
     context.stroke();*/
 
     context.beginPath();
+    let saedRadius = 1000;
+    if(saedInserted){
+      saedRadius = saedSizes[saedLevel - 1];
+    }
+    context.arc(this.selector[0].width / 2 + saedx, this.selector[0].height / 2 + saedy, Math.sqrt(saedRadius) * this.zooms[this.mag] / 15000, 0, Math.PI * 2);
+    context.clip();
+
+    context.beginPath();
     context.arc(this.maskX + c2x * this.intensity + this.haloX * Math.cos(this.beamAngle) + this.haloY * Math.sin(this.beamAngle), this.maskY + c2y * this.intensity + this.haloY * Math.cos(this.beamAngle) - this.haloX * Math.sin(this.beamAngle), haloR / 4, 0, Math.PI * 2);
     context.strokeStyle = 'rgba(255,255,255,' + Math.min(1, 10 / haloR) + ')';
     context.lineWidth = this.calculateHaloLineWidth(haloR);
