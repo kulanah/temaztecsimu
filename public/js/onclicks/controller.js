b5a0c091b7814c59a0c9f1c93af9c702cadc5e7b
kulanah <devonthebutler@gmail.com>
2018-04-03 12:18:54 -0700

    made updates to sample placement and min/max based on opne image

--- public/js/onclicks/controller.js
+++ public/js/onclicks/controller.js
@@ -353,7 +353,7 @@ let microscopeControllers = function(){
   });
 
   $('#buttonupr').on('click', function(event){
-    if (temLensSpecimen.y < 480 - stageStepSize){
+    if (temLensSpecimen.y < 470 - stageStepSize){
       focusUp();
       temLensSpecimen.y += stageStepSize;
       drawColumn();
@@ -363,7 +363,7 @@ let microscopeControllers = function(){
   });
 
   $('#buttondownr').on('click', function(event){
-    if (temLensSpecimen.y > 460 + stageStepSize){
+    if (temLensSpecimen.y > 443 + stageStepSize){
       focusDown();
       temLensSpecimen.y -= stageStepSize;
       drawColumn();
