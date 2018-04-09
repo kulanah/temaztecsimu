0a02eeae4953bc9148074f987be55f8105decc83
Noah Freed <noah25@psu.ds.pdx.edu>
2018-04-09 16:12:48 -0700

    Speaking stops if the window is not visible

--- public/js/textToSpeech.js
+++ public/js/textToSpeech.js
@@ -1569,3 +1569,33 @@ $('#speakernotespause').on('click', function(){
 $('#speakernotesstop').on('click', function(){
     window.speechSynthesis.cancel();
 });
+
+// Stop speaking if the window is not visible
+// Visibility change code based on example in documentation at https://developer.mozilla.org/en-US/docs/Web/API/Page_Visibility_API
+var hidden, visibilityChange; 
+if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support 
+  hidden = "hidden";
+  visibilityChange = "visibilitychange";
+} else if (typeof document.msHidden !== "undefined") {
+  hidden = "msHidden";
+  visibilityChange = "msvisibilitychange";
+} else if (typeof document.webkitHidden !== "undefined") {
+  hidden = "webkitHidden";
+  visibilityChange = "webkitvisibilitychange";
+}
+
+// If the page is hidden, pause the video;
+// if the page is shown, play the video
+function handleVisibilityChange() {
+    if (document[hidden]) {
+        window.speechSynthesis.cancel();
+    }
+}
+  
+// Warn if the browser doesn't support addEventListener or the Page Visibility API
+if (typeof document.addEventListener === "undefined" || typeof document.hidden === "undefined") {
+    console.log("This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API.");
+} else {
+    // Handle page visibility change   
+    document.addEventListener(visibilityChange, handleVisibilityChange, false);
+}
